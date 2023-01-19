453ab0c0b1b1f0b9e383a2d33dc10173730aa221
Ben Edwards <ben-edwards@hotmail.co.uk>
2023-01-19 20:40:43 +0000

    get and post

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000 || process.env.PORT;
+let tourData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.use(express.json());
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    count: tourData.length,
+    data: {
+      tours: tourData,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tourId = Number(req.params.id);
+  const tour = tourData.find((obj) => obj.id === tourId);
+
+  if (!tour) {
+    return res
+      .status(404)
+      .json({ status: 'failed', message: 'Invalid Tour ID' });
+  }
+  if (tourId <= tourData.length)
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tours: tour,
+      },
+    });
+  esl;
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // Work out the last ID and increment by 1
+  const newId = tourData[tourData.length - 1].id + 1;
+
+  // Add the new ID to the request JSON
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  // Add to the tourData array
+  tourData.push(newTour);
+
+  // Write back out to file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tourData),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}.`);
+});
